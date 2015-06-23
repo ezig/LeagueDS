8e38cc93bc76fcd23b826ade77a07c5919a97399
Ezra <ezrazigmond@gmail.com>
2015-06-23 19:35:32 -0400

    Reduced dependencies of api.py, got rid of functions that were only being used in one place

--- api.py
+++ api.py
@@ -1,31 +1,24 @@
-from lxml import html
 import requests
-import ast
-import urllib 
 import urllib2
-import simplejson
 
 api_key = "5812433f-6e68-4c43-973f-9a5d21d50503"
 
-def url_gen1(name):
-	name = urllib.quote(name,'')
-	URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name +"?api_key=" + api_key
-	return URL 
-
 def summonerID(name):
-	page = requests.get(url_gen1(name))
-	temp = ast.literal_eval(page.content)
-	name = name.replace(' ', '').lower()
-	summonerID = temp[name]['id']
-	return summonerID
+	# set up the URL
+	name = urllib2.quote(name,'')
+	url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + name +"?api_key=" + api_key
 
-def url_gen2(name):
-	s_id = str(summonerID(name))
-	URL = "https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + s_id + "?rankedQueues=RANKED_SOLO_5x5&api_key=" + api_key
-	return URL
+	response = requests.get(url).json()
+	name = name.replace(' ', '').lower()
+
+	# return the summoner ID
+	return response[name]['id']
 
 def accountID(name):
-	json_obj = urllib2.urlopen(url_gen2(name))
-	data = simplejson.load(json_obj)
-	URI = data['matches'][0]['participantIdentities'][0]['player']['matchHistoryUri']
-	return URI
+	s_id = str(summonerID(name))
+	url = "https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + s_id + "?rankedQueues=RANKED_SOLO_5x5&api_key=" + api_key
+
+	response = requests.get(url).json()
+
+	# return the URI
+	return str(response['matches'][0]['participantIdentities'][0]['player']['matchHistoryUri'])
