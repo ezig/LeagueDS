ab71a447538273e4c6de6fd24aec8600c5eb1f37
Ezra <ezrazigmond@gmail.com>
2015-06-23 19:21:22 -0400

    Reduced the dependencies of mmr.py and fixed error handling (literal_eval was throwing uncaught exceptions in error cases)

--- mmr.py
+++ mmr.py
@@ -1,5 +1,3 @@
-from lxml import html
-import ast 
 import requests
 import urllib2
 
@@ -7,19 +5,17 @@ import urllib2
 def url_gen(name):
 	template = 'http://na.op.gg/summoner/ajax/mmr.json/userName='
 	# escapes the space character and adds %20
-	name = urllib.quote(name,'')
+	name = urllib2.quote(name,'')
 	URL = template + name
 	return URL
 
 # to test, use either "jo1345" or "NME Flaresz x"
 def mmr_return(name):
-	page = requests.get(url_gen(name))
-	temp = ast.literal_eval(page.content)
-	if 'error' in temp.keys():
+	response = requests.get(url_gen(name)).json()
+	if 'error' in response:
 		return None
 	else:	
-		mmr = temp['mmr'].replace(',','')
+		mmr = response['mmr'].replace(',','')
 		return int(mmr)
 
-	# 	2(name))
-	# data = simplejson.load(json_obj)
+print mmr_return("Valos4")
